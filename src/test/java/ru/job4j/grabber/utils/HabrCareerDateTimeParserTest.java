d41b6f7d3d6578cc16eb44341a72107110c8e62c
SweetMustard993 <bogdanasdaq@gmail.com>
2023-08-02 17:30:23 +0300

    2.1. Преобразование даты [#289476 # [#289476 #433630]]

---
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.grabber.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HabrCareerDateTimeParserTest {
+
+    @Test
+    void parseWhenDateContainsTimeZone() {
+        String datetime = "2023-07-31T15:57:41+03:00";
+        DateTimeParser dtp = new HabrCareerDateTimeParser();
+        LocalDateTime res = dtp.parse(datetime);
+        assertEquals("2023-07-31T15:57:41", res.toString());
+    }
+
+    @Test
+    void parseWhenDateWithoutTimeZone() {
+        String datetime = "2023-07-31T15:57:41";
+        DateTimeParser dtp = new HabrCareerDateTimeParser();
+        LocalDateTime res = dtp.parse(datetime);
+        assertEquals("2023-07-31T15:57:41", res.toString());
+    }
+
+    @Test
+    void parseWhenDateContainsMiliSec() {
+        String datetime = "2023-07-31T15:57:41.926";
+        DateTimeParser dtp = new HabrCareerDateTimeParser();
+        LocalDateTime res = dtp.parse(datetime);
+        assertEquals("2023-07-31T15:57:41", res.toString());
+    }
+}
